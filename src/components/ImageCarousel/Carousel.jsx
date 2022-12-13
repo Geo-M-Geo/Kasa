b97fb43aee79c7fedb3a92f9637432a6362a9824
Geoffrey <78374883+Geo-M-Geo@users.noreply.github.com>
2022-12-13 21:08:59 +0100

    Debut du composant carousel

--- src/components/ImageCarousel/Carousel.jsx
+++ src/components/ImageCarousel/Carousel.jsx
@@ -2,16 +2,46 @@ import { useState } from 'React';
 import { Link } from 'react-router-dom'
 import { appartements } from '../../datas/appartements';
 
-function Carousel() {
+function Carousel({appartements}) {
     const [currentIndex, setIndex] = useState(0);
-    const appartementPicture = parseInt(appartements)
-    return (
 
-        <div className="carousel-container">
-            <img src=${appartementPictures[currentIndex]} className="carousel-container__image" />
-            <Link className="carousel-container__image_back" onClick={() => setIndex(-1)}/>
-            <Link className="carousel-container__image_foward" onClick={() => setIndex(1)}/>
-        </div>
+    const prevPicture = () => {
+        setIndex(currentIndex === 1 ? 1 : currentIndex - 1)};
+        
+    const nextPicture = () => {
+        setIndex(currentIndex + 1)};
 
-    )
-}
+        return (
+            
+            <div>
+            {appartements.map((pictures, index) => {
+                return  (
+                <div>
+                <div key={index}>
+                    {index === currentIndex && ( <img src={pictures} alt='' />)}
+                </div>
+                <Link className="carousel-container__image_back" onClick={(prevPicture)}/>
+                <Link className="carousel-container__image_foward" onClick={(nextPicture)}/>
+                </div>
+                )
+            })}
+            </div>
+        )
+    }
+
+    export default Carousel
+
+
+
+
+//             <div className="carousel-container">
+    
+//             {appartements.map((appartements, index) => {
+//                 return  <img key={index}>{appartements.pictures[currentIndex]}</img>
+//             })}
+//             <Link className="carousel-container__image_back" onClick={() => setIndex(-1)}/>
+//             <Link className="carousel-container__image_foward" onClick={() => setIndex(1)}/>
+//         </div>
+//     )
+    
+// }
