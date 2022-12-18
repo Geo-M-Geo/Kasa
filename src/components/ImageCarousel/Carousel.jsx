ab72dac6100d98fef75ab18e1f6c71a76e0b22f9
Geoffrey <78374883+Geo-M-Geo@users.noreply.github.com>
2022-12-18 20:15:01 +0100

    debut de la page logement

--- src/components/ImageCarousel/Carousel.jsx
+++ src/components/ImageCarousel/Carousel.jsx
@@ -1,32 +1,37 @@
-// import React from "react";
-// import { useState } from 'React';
-// // import { appartements } from '../../datas/appartements';
-// import  vector_foward  from '../../assets/vector_foward.png';
-// import  vector_back  from '../../assets/vector_back.png';
+import React from 'react'
+import { useState } from 'react';
+import { useParams } from 'react-router-dom'
+import { appartements } from '../../datas/appartements';
+import  vector_foward  from '../../assets/vector_foward.png';
+import  vector_back  from '../../assets/vector_back.png';
 
 
-// function Carousel({ appartements }) {
-//     const [currentIndex, setCurrentIndex] = useState(0);
-//     const length = appartements.length;
+function Carousel() {
+    const AppartementId = useParams();
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const length = appartements.length;
+    const NextPicture = currentIndex === length ? 0 : currentIndex + 1;
+    const PreviousPicture = currentIndex === 0 ? length -1 : currentIndex - 1;
+    const TheAppartment = appartements[currentIndex]
+
+    return(
+        <div className="carouselcontainer">
+            console.log({AppartementId})
+        </div>
+    ) 
 
 
+    
+    
+} 
+export default Carousel;
 
-//     return(
-//         <div className="carouselcontainer">
-//             {appartements.map((picture, index) => {
-//                 return (
-//                     <div>
-//                         <img alt="" key={index}>{picture}</img>
-//                         <img alt="" to={vector_foward} className="carousel-container__image_back" onClick={setCurrentIndex(currentIndex === length ? 0 : currentIndex + 1) }/>
-//                         <img alt="" to={vector_back} className="carousel-container__image_foward" onClick={(setCurrentIndex(currentIndex === 0 ? length -1 : currentIndex - 1))}/>
-//                     </div>
-//                 )
-//             })}
+// {appartements.map((picture, index) => {
+//     return (
+//         <div>
+//             <img alt="" key={index}>{picture}</img>
+//             <img alt="" to={vector_foward} className="carousel-container__image_back" onClick={setCurrentIndex(NextPicture) }/>
+//             <img alt="" to={vector_back} className="carousel-container__image_foward" onClick={setCurrentIndex(PreviousPicture)}/>
 //         </div>
-//     ) 
-
-
-    
-    
-// } 
-// export default Carousel;
+//     )
+// })}
--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,18 +1,18 @@
 // import { useParams } from 'react-router-dom';
 // import { appartements } from '../../datas/appartements'
-// import Carousel from '../../components/ImageCarousel/Carousel'
-// import Collapse from '../../components/Collapse/Collapse';
+import Carousel from '../../components/ImageCarousel/Carousel'
+import Collapse from '../../components/Collapse/Collapse';
 // import LogementInfos from '../../components/LogementInfos/LogementInfos'
 
-// function Logement() {
+function Logement() {
 
-//     return ( 
-//         <div>
-//             <Carousel />
-//             <LogementInfos id={id} />
-//             <Collapse />
-//         </div>
-//     )
-// }
+    return ( 
+        <div>
+            <Carousel />
+            {/* <LogementInfos id={id} /> */}
+            {/* <Collapse /> */}
+        </div>
+    )
+}
 
-// export default Logement
+export default Logement
